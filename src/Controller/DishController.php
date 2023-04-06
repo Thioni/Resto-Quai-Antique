5d46332a8405efd4dcd399f4e4bdf3fdfa770971
Thierry Castiglioni <castth@hotmail.com>
2023-04-06 18:03:00 +0200

    Dish create and form

--- src/Controller/DishController.php
+++ src/Controller/DishController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Dish;
+use App\Form\DishType;
 use App\Repository\DishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,6 +23,24 @@ class DishController extends AbstractController {
         $dishes = $repo->findAll();
         return $this->render('dish/list.html.twig', [
             "dishes" => $dishes,
+        ]);
+    }
+
+    #[Route("/create-dish", name: "create_dish")]
+    public function create(Request $request): Response {
+        $dish = new Dish();
+
+        $form = $this->createForm(DishType::class, $dish);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Dump temporaire à supprimer //
+            dump($dish);
+            return $this->redirectToRoute("dish_list");
+        }
+
+        return $this->render('dish/create.html.twig', [
+            "form" => $form
         ]);
     }
 
---
+++ src/Form/DishType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class DishType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, ['label' => "Nom"])
+            ->add('description', TextType::class, ['label' => "Déscription"])
+            ->add('price', NumberType::class, ['label' => "Prix"])
+            ->add('category', ChoiceType::class, [
+                'label' => "Catégorie",
+                'choices' => [
+                    "Entrée" => "Entrée",
+                    "Plat" => "Plat",
+                    "Dessert" => "Dessert",
+                    "Vin" => "Vin",
+                    "Boisson" => "Boisson",
+                ] 
+            ])
+        ;
+    }
+
+};
