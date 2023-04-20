e0dfacdb82a625dc6f25c1bb68c2f890b4604fff
Thierry Castiglioni <castth@hotmail.com>
2023-04-20 17:29:37 +0200

    Updated BookingType and BookingController

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Booking;
 use App\Form\BookingType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,11 +13,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class BookingController extends AbstractController {
 
     #[Route("/user-booking", name: "user_booking")]
-    public function createBooking(Request $request): Response {
+    public function createBooking(Request $request, ManagerRegistry $doctrine): Response {
         $booking = new Booking();
 
         $form = $this->createForm(BookingType::class, $booking);
         $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            dump($booking);
+            // $em = $doctrine->getManager();
+            // $em->persist($booking);
+            // $em->flush();
+            // $this->addFlash('success', 'Réservation validée.');
+            // return $this->redirectToRoute("dish_selection");
+        }
 
         return $this->render('booking/create.html.twig', [
             "form" => $form
--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -5,8 +5,10 @@ namespace App\Form;
 use App\Entity\Allergen;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class BookingType extends AbstractType {
@@ -16,9 +18,17 @@ class BookingType extends AbstractType {
         $builder
             ->add('firstname', TextType::class, ['label' => "Prénom"])
             ->add('lastname', TextType::class, ['label' => "Nom"])
-            ->add('email', TextType::class, ['label' => "Adresse mail"])
-            ->add('seats', TextType::class, ['label' => "Nombre de personnes"])
-            ->add('timeslot', TimeType::class, ['label' => "Heure de réservation"])
+            ->add('email', EmailType::class, ['label' => "Adresse mail"])
+            ->add('seats',  IntegerType::class, [
+                'label' => "Nombre de personnes",
+                'attr' => ['class' => 'text-center'],
+                ])
+            ->add('timeslot', DateTimeType::class, [
+                'label' => "Heure de réservation",
+                'minutes' => range(0, 45, 15),
+                'data' => new \DateTime(),
+                "years" => range(date('Y'), date('Y') +1),                
+            ])
             ->add('allergy', EntityType::class, [
                 'class' => Allergen::class,
                 'label' => "Allergies",
@@ -26,7 +36,11 @@ class BookingType extends AbstractType {
                 'multiple' => true,
                 'expanded' => true,
             ])
-            ->add('comment', TextType::class, ['label' => "Commentaires"])
+            ->add('comment', TextType::class, [
+                'label' => "Commentaires",
+                'required' => false,
+                'attr' => ['style' => 'height: 150px'],
+                ])
         ;
     }
     
