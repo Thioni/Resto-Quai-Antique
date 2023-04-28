5189ff8403f3a0e6675b5c8744b2c8269364f5ab
Thierry Castiglioni <castth@hotmail.com>
2023-04-28 15:25:18 +0200

    Added password hash

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,21 +8,30 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController {
 
     #[Route("/create-user", name: "create_user")]
-    public function createBooking(Request $request, ManagerRegistry $doctrine): Response {
+    public function createBooking(Request $request, ManagerRegistry $doctrine, UserPasswordHasherInterface $userPasswordHasher): Response {
         $user = new User();
 
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // $em = $doctrine->getManager();
-            // $em->persist($user);
-            // $em->flush();
+
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                  $user,
+                  $form->get('password')->getData()
+                )
+              );
+
+            $em = $doctrine->getManager();
+            $em->persist($user);
+            $em->flush();
             $this->addFlash('success', 'Mail de confirmation envoyé');
             return $this->redirectToRoute("app_login");
         }
